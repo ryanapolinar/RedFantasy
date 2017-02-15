3ccd58e786484e59bf5edd187ba11036cbd9bc25
Rikerd <cursedmarkedsasuke@gmail.com>
2017-02-15 14:47:38 -0800

    Prototype Pause Menu
    Beginnning Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject menu;
+    public bool isShowing;
+
+    // Use this for initialization
+    void Start()
+    {
+        menu.SetActive(isShowing);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isShowing = !isShowing;
+            menu.SetActive(isShowing);
+            if (isShowing)
+            {
+                Time.timeScale = 0f;
+            }
+
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PauseSelector.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseSelector : MonoBehaviour {
+    public GameObject test; // will change
+    public GameObject menu; // will change
+
+    private Vector3 position;
+    private string[] commands = new string[2] { "resume", "main"};
+    private int command_index = 0;
+    public float move_val = 4.5f;
+
+    // Use this for initialization
+    void Start () {
+        position = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        string command = commands[command_index];
+        if (Input.GetKeyDown("z"))
+        {
+            if (command == "resume")
+            {
+                Time.timeScale = 1.0f;
+                test.GetComponent<PauseMenu>().isShowing = false;
+                menu.SetActive(test.GetComponent<PauseMenu>().isShowing);
+            }
+            else if (command == "main")
+            {
+                SceneManager.LoadScene("Main Menu");
+            }
+        }
+    }
+
+    //Shifts selector up/down depending on input
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ShiftUp();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ShiftDown();
+        }
+    }
+
+    void ShiftUp()
+    {
+        if (command_index > 0)
+        {
+            position.y += move_val;
+            transform.position = position;
+            command_index -= 1;
+        }
+        else
+        {
+            position.y -= (move_val);
+            transform.position = position;
+            command_index = 3;
+        }
+    }
+
+    void ShiftDown()
+    {
+        if (command_index < 1)
+        {
+            position.y -= move_val;
+            transform.position = position;
+            command_index += 1;
+        }
+        else
+        {
+            position.y += (move_val);
+            transform.position = position;
+            command_index = 0;
+        }
+    }
+}
